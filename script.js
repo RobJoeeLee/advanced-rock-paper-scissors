e1257b71a89f559a6c4a6ee6038d9b83c0f7340d
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-01-06 11:28:06 -0500

    Created a function called makeSelection that is called when a selection is clicked on

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const selectionButtons = document.querySelectorAll("[data-selection]")
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener("click" , e => {
         selectionName = selectionButton.dataset.selection
+        makeSelection(selectionName)
     })
 })
+
+function makeSelection(selectionName){
+    console.log(selectionName)
+}
